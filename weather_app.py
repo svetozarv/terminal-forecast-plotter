e34188c7bbf37716ae990f97e79a64eadb1a5463
svetozarv <svetozar.volskyi@gmail.com>
2025-12-15 00:01:27 +0100

    add constructor and bacis get_from_api()

--- weather_app.py
+++ weather_app.py
@@ -1,26 +1,44 @@
 import time
 from dataclasses import dataclass
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
 
 # https://open-meteo.com/en/docs
 # 1. first version will be using terminal to communicate with user
 # 2. settings will be saved to a file (with login and authentication?)
 # 3. while executing a program, user's inputs will be parsed using input()
 
-class WeatherApp:
-    pass
-
 
 class WeatherApp:
     def __init__(self):
-        pass
+        """
+        Setup the Open-Meteo API client with cache and retry on error
+        """
+        self._url = "https://api.open-meteo.com/v1/forecast"
+        self._cache_session = requests_cache.CachedSession('.cache', expire_after=3_600)
+        self._retry_session = retry(self._cache_session, retries=5, backoff_factor=0.2)
+        self._openmeteo = openmeteo_requests.Client(session=self._retry_session)
 
     def _get_data_from_api(self, latitude, longitude, elevation, timezone, start_date, end_date):
         """
         a city/region or coordinates must be provided
         """
-        pass
+        
+        # Make sure all required weather variables are listed here
+        # The order of variables in hourly or daily is important to assign them correctly below
+        params = {
+            "latitude": 52.52,
+            "longitude": 13.41,
+            "hourly": "temperature_2m",
+        }
+        responses = self._openmeteo.weather_api(self._url, params=params)
 
-    
+        # Process first location. Add a for-loop for multiple locations or weather models
+        response = responses[0]
+        print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+        print(f"Elevation: {response.Elevation()} m asl")
+        print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
     def create_alert(self):
         """
