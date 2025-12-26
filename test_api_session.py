fa5fdfcf0269872294b6ab33486a663239503fdd
svetozarv <svetozar.volskyi@gmail.com>
2025-12-26 22:53:54 +0100

    add test templates

--- test_api_session.py
+++ test_api_session.py
@@ -1,9 +1,6 @@
-from api_session import *
-from helpers import *
+from api_session import ApiSession
+# from helpers import *
 
-
-def test_coords_to_city_name():
-    assert coords_to_city_name(52.2297, 21.0122) == "Warszawa, Polska"
 
 def test_openmeteo_api_up():
     assert ApiSession()._make_api_call(None, None)
---
+++ test_my_weather_app.py
@@ -0,0 +1,1 @@
+from my_weather_app import MyWeatherApp, Plotter, make_data_payload
