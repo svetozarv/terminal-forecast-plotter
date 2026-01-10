7be5130f928e2177e296a0069c25fa2555f14cb8
svetozarv <svetozar.volskyi@gmail.com>
2026-01-10 01:13:18 +0100

    add logger dialog pop up, ability to add alert wind speed and name

--- terminal_user_interface.py
+++ terminal_user_interface.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
 
-logging.getLogger(__name__)
+logging.getLogger("terminal_user_interface")
+logging.basicConfig(filename='terminal_user_interface.log', level=logging.INFO, filemode="w+")
 import peewee as pw
 from textual import on, work
 from textual.app import App, ComposeResult
@@ -10,6 +11,7 @@ from textual.containers import Center, Grid, HorizontalGroup, VerticalScroll
 from textual.events import ScreenResume, ScreenSuspend
 from textual.screen import ModalScreen, Screen
 from textual.widgets import (
+    Button,
     DataTable,
     Footer,
     Header,
@@ -23,8 +25,14 @@ from textual.widgets import (
 )
 from textual_plotext import PlotextPlot
 
-from database_orm import DATABASE_FILENAME, Alert
-from geocoder import Location
+from database_orm import (
+    DATABASE_FILENAME,
+    MAX_TEMP,
+    MAX_WINDSPEED,
+    MIN_TEMP,
+    MIN_WINDSPEED,
+    Alert,
+)
 from my_weather_app import MyWeatherApp
 
 
@@ -32,7 +40,7 @@ class MainScreen(Screen):
     def compose(self) -> ComposeResult:
         # TODO: Welcome label
         yield Placeholder("MainScreen")
-        yield Label("Alert has been triggered!\n", id="alert_triggered_label")
+        yield Label("Alert has been triggered!\n", id="alert_has_beeb_triggered_label")
         yield Label("", id="alerts_label")
         yield Footer()
 
@@ -47,21 +55,20 @@ class MainScreen(Screen):
 
     def check_alerts(self):
         for alert in list(Alert.select()):
-            self.check_alert_for_city(alert.city_name, alert.min_temp, alert.max_temp)
+            self.check_alert_for_city(alert)
 
-    def check_alert_for_city(self, city: str, min_temp: float, max_temp: float):
-        location = Location(city_prompt=city)       # bottleneck to remove
-        current_weather = app.my_weather_app.get_current_weather(location)
+    def check_alert_for_city(self, alert: pw.BaseModelSelect):
+        current_weather = app.my_weather_app.get_current_weather((alert.lat, alert.lon))
         current_temp = current_weather.temperature_2m
         alerts_label = self.screen.query_one("#alerts_label", Label)
         alert_triggered_label = self.screen.query_one("#alert_triggered_label", Label)
         label_text = alerts_label.content
-        if current_temp < min_temp:
-            label_text += f"The temperature for {location.city_name} has dropped below {min_temp}°C\n"
+        if current_temp < alert.min_temp:
+            label_text += f"The temperature for {alert.city_name} has dropped below {alert.min_temp}°C\n"
             alert_triggered_label.display = True
-        if current_temp > max_temp:
+        if current_temp > alert.max_temp:
             alert_triggered_label.display = True
-            label_text += f"The temperature for {location.city_name} has raised above {max_temp}°C\n"
+            label_text += f"The temperature for {alert.city_name} has raised above {alert.max_temp}°C\n"
         alerts_label.update(label_text)
 
 
@@ -79,6 +86,7 @@ class AskForCityScreen(Screen):
         yield Footer()
 
     def get_city_prompt(self) -> str:
+        # TODO: add validataion, results first, then show if results not None else display msg
         app.city_prompt = self.query_one(Input).value
         return app.city_prompt
 
@@ -86,27 +94,74 @@ class AskForCityScreen(Screen):
         self.query_one(Pretty).update(self.get_city_prompt())
         app.switch_screen("plot")
 
+class DialogPopupScreen(ModalScreen):    # TODO:
+    def compose(self) -> ComposeResult:
+        with Center():
+            yield Label(app.dialog_popup_text)
+            yield Button("OK")
+
+    @on(Button.Pressed)
+    def close(self):
+        app.pop_screen()
 
 class AskAlertDetailsScreen(ModalScreen):
     def compose(self) -> ComposeResult:
-        dialog_message = "Please, provide the temperatures for alert to trigger." + \
-            "You have to fill at least one field."
+        dialog_message = "Please, provide the temperatures for alert to trigger. " + \
+            "You can leave some fields empty."
         yield Grid(
-            Label(dialog_message, id="askforalert_dialog"),
-            Input(placeholder="Min. temp.", type='number', id="min_temp_input"),
-            Input(placeholder="Max. temp.", type='number', id="max_temp_input"),
-            id="dialog",
+            Label(dialog_message, id="ask_for_alert_dialog"),
+            Input(placeholder="Name/description", id="name", valid_empty=True),
+            Input(placeholder="Min. temp. °C", type='number', id="min_temp_input", valid_empty=True),
+            Input(placeholder="Max. temp. °C", type='number', id="max_temp_input", valid_empty=True),
+            Input(placeholder="Min. wind speed, m/s", type='number', id="min_windspeed_input", valid_empty=True),
+            Input(placeholder="Max. wind speed, m/s", type='number', id="max_windspeed_input", valid_empty=True),
+            id="ask_for_alert_grid",
         )
+        # TODO: add validation for min and max temps and chars in the input (in can be "")
 
     def add_alert(self):
-        min_temp = self.screen.query_one("#min_temp_input").value
-        max_temp = self.screen.query_one("#max_temp_input").value
-        row = Alert.update(min_temp=min_temp, max_temp=max_temp).where(
-            Alert.city_name == app.my_weather_app.current_location.city_name
-        ).execute()
+        name = self.screen.query_one("#name", Input).value
+        min_temp = self.screen.query_one("#min_temp_input", Input).value
+        max_temp = self.screen.query_one("#max_temp_input", Input).value
+        min_wind_speed = self.screen.query_one("#min_windspeed_input", Input).value
+        max_wind_speed = self.screen.query_one("#max_windspeed_input", Input).value
 
-    def on_input_submitted(self):
+        # if user hasn't provided values (they are "")
+        if not name:
+            name = "-"
+        if not min_temp:
+            min_temp = MIN_TEMP
+        if not max_temp:
+            max_temp = MAX_TEMP
+        if not min_wind_speed:
+            min_wind_speed = MIN_WINDSPEED
+        if not max_wind_speed:
+            max_wind_speed = MAX_WINDSPEED
+        logging.info(f"Values: {name} {min_temp} {max_temp} {min_wind_speed} {max_wind_speed}")
+
+        if name == "^Q":  # user pressed 'exit' while in the screen
+            logging.info("Detected exit input. add_alert aborted.")
+            return
+        if not min_temp and not max_temp and not min_wind_speed and not max_wind_speed:
+            logging.info("No values provided. add_alert aborted.")
+            return
+        Alert.get_or_create(
+            name=name,
+            city_name=app.my_weather_app.current_city_name(),
+            lat=app.my_weather_app.current_coords[0],
+            lon=app.my_weather_app.current_coords[1],
+            min_temp=min_temp,
+            max_temp=max_temp,
+            min_wind_speed=min_wind_speed,
+            max_wind_speed=max_wind_speed,
+        )
+        logging.info("Alert was added.")
+
+    @on(Input.Submitted)
+    def process_submit(self):
         self.add_alert()
+        app.dialog_popup_text = "Saved!"
+        app.switch_screen("dialog_popup")
         app.pop_screen()
 
 
@@ -131,19 +186,17 @@ class PlotScreen(Screen):
         self.screen.loading = True
         self.draw_hourly()
 
-    @work
-    async def draw_hourly(self):
+    def draw_hourly(self):
         plt = self.query_one(PlotextPlot).plt
-        await app.my_weather_app.draw_hourly_plot(plt, app.city_prompt)
+        app.my_weather_app.draw_hourly_plot(plt, app.city_prompt)
         self.query_one(PlotextPlot).refresh()
         self.screen.loading = False
         self.displaying_daily = False
         self.displaying_hourly = True
 
-    @work
-    async def draw_daily(self):
+    def draw_daily(self):
         plt = self.query_one(PlotextPlot).plt
-        await app.my_weather_app.draw_daily_plot(plt, app.city_prompt)
+        app.my_weather_app.draw_daily_plot(plt, app.city_prompt)
         self.query_one(PlotextPlot).refresh()
         self.screen.loading = False
         self.displaying_daily = True
@@ -155,9 +208,15 @@ class PlotScreen(Screen):
 
     def action_save_to_favourties(self):
         """Handles keybinding."""
-        if Alert.get_or_none(Alert.city_name == app.my_weather_app.current_location.city_name):
+        if Alert.get_or_none(Alert.city_name == app.my_weather_app.current_city_name()):
+            app.display_dialog("The city is already in your favourites.")
             return
-        Alert(city_name=app.my_weather_app.current_location.city_name).save()
+        Alert(
+            city_name=app.my_weather_app.current_city_name(),
+            lat=app.my_weather_app.current_coords[0],
+            lon=app.my_weather_app.current_coords[1],
+        ).save()
+        app.display_dialog("Saved!")
         # self.screen.styles.background = "lime"
         # self.screen.styles.animate("opacity", value=0.0, duration=1.0)
 
@@ -181,6 +240,7 @@ class FavouritesScreen(Screen):
 
     @on(ScreenResume)
     def on_mount(self):
+        # get a list of all city names as strings from db
         favourites = list(map(lambda favourite: favourite.city_name, Alert.select(Alert.city_name)))
         label = self.screen.query_one(Label)
         if not favourites:
@@ -201,7 +261,16 @@ class FavouritesScreen(Screen):
 
 
 class AlertsScreen(Screen):
-    COLUMNS = ["City", "Min. temp", "Max. temp"]
+    COLUMNS = [
+        "№",
+        "City",
+        "Alert name",
+        "Severity",
+        "Min. temp",
+        "Max. temp",
+        "Min. wind speed",
+        "Max. wind speed",
+    ]
     label = "These are your saved alerts:"
 
     def compose(self) -> ComposeResult:
@@ -212,18 +281,28 @@ class AlertsScreen(Screen):
 
     @on(ScreenResume)
     def update_rows(self):
-        data_table = self.screen.query_one(DataTable)
+        placeholder = "-not set-"
+        data_table = self.screen.query_one("#alerts_data_table", DataTable)
         data_table.clear()
         alerts = list(
             map(
-                lambda a: (a.city_name, a.min_temp, a.max_temp),
-                Alert.select(Alert.city_name, Alert.min_temp, Alert.max_temp),
+                lambda a: (
+                    a.id,
+                    a.city_name,
+                    a.name,
+                    a.severity,
+                    a.min_temp if a.min_temp > MIN_TEMP else placeholder,
+                    a.max_temp if a.max_temp < MAX_TEMP else placeholder,
+                    a.min_wind_speed if a.min_wind_speed > MIN_WINDSPEED else placeholder,
+                    a.max_wind_speed if a.max_wind_speed < MAX_WINDSPEED else placeholder,
+                ),
+                Alert.select(),
             )
         )
         data_table.add_rows(alerts)
 
     def on_mount(self):
-        data_table = self.screen.query_one(DataTable)
+        data_table = self.screen.query_one("#alerts_data_table", DataTable)
         data_table.add_columns(*self.COLUMNS)
         self.update_rows()
 
@@ -241,6 +320,7 @@ class TerminalUserInterface(App):
     db = pw.SqliteDatabase(DATABASE_FILENAME)
     db.connect()
     city_prompt = None  # to store the city name entered by user between screens
+    dialog_popup_text = None
 
     CSS_PATH = "terminal_user_interface.tcss"
     BINDINGS = [
@@ -249,6 +329,7 @@ class TerminalUserInterface(App):
         ("a", "switch_to_screen('alerts')", "Alerts"),
         Binding("m, escape", "switch_to_screen('main')", "Return to main", priority=True),
         ("d", "toggle_dark", "Toggle dark mode"),
+        Binding("ctrl+q", "quit", "Quit", show=False, priority=True),
     ]
     SCREENS = {
         "main": MainScreen,
@@ -257,6 +338,7 @@ class TerminalUserInterface(App):
         "ask_for_city": AskForCityScreen,
         "plot": PlotScreen,
         "ask_alert_details": AskAlertDetailsScreen,
+        "dialog_popup": DialogPopupScreen,
     }
 
     def compose(self) -> ComposeResult:
@@ -272,6 +354,15 @@ class TerminalUserInterface(App):
     def action_switch_to_screen(self, name):
         self.switch_screen(name)
         self.refresh_bindings()
+
+    def display_dialog(self, text):
+        """Displays a modal screen with text."""
+        self.dialog_popup_text = text
+        self.push_screen("dialog_popup")
+
+    def action_quit(self):
+        app.db.close()
+        return super().action_quit()
 
     def check_action(self, action, parameters):
         if isinstance(self.screen, MainScreen):
@@ -305,7 +396,6 @@ class TerminalUserInterface(App):
 
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='tui.log', level=logging.INFO)
     app = TerminalUserInterface()
     app.run()
     app.db.close()
